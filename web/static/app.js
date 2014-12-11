f0f375a3c69255ad46b5fa441c74daee1b4389ed
Emnet <emnetg@uw.edu>
2014-12-10 20:36:27 -0800

    mapping almost works

--- web/static/app.js
+++ web/static/app.js
@@ -2,8 +2,28 @@
 
 $(document).ready(function() {
 
-    var polygon = $('#my-data').data();
+    var polygon = $('#my-data').data('bound');
+    console.log(polygon.type);
 
-    console.log(polygon)
+    function createMap(center, zoom) {
+
+        var mapElem = document.getElementById('map');
+
+        var map = new google.maps.Map(mapElem, {
+            center: center,
+            zoom: zoom
+        });
+
+        $(window).resize(function() {
+            $('#map').css('height', $(window).height - $('#map').position().top - 20);
+        });
+    }
+
+    var areaCoords = {
+        lat: 41.8,
+        lng: -87.6
+    };
+
+    createMap(areaCoords, 12);
 
 });
